a0992ba08f915e6eac252c5e48409d8f2ce4e373
Evan <evanamiesgalonski@gmail.com>
2018-11-21 15:24:25 -0800

    use desc package to import Imports list

--- R/loadpkgs.R
+++ R/loadpkgs.R
@@ -1,17 +1,14 @@
 .onAttach <- function(...) {
-
-  pkgs <- read.delim("DESCRIPTION", header = F)
-  start <- grep("Imports:", pkgs$V1) + 1
-  end <- grep("Suggests:", pkgs$V1) - 1
-  pkgs <- pkgs[start:end, ]
-  pkgs <- paste(gsub(",", "", pkgs))
+  library(desc)
+  pkgs <- desc_get_field("Imports", "DESCRIPTION")
+  pkgs <- paste(gsub("\\n", "", pkgs))
   pkgs <- paste(gsub(" ", "", pkgs))
+  pkgs <- strsplit(pkgs, ",")[[1]]
 
   needed <- pkgs[is_attached(pkgs) == F]
 
  if (length(needed) == 0)
    return()
-
 
   packageStartupMessage("Loading poispkgs...")
   suppressPackageStartupMessages(lapply(pkgs, library, character.only = TRUE, warn.conflicts = T))
@@ -28,6 +25,3 @@
 is_attached <- function(x) {
   paste0("package:", x) %in% search()
 }
-
-
-
