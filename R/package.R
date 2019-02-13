7d376420bb5cc5d9b6f3ee529a50c425f761b823
Joe Thorley <joe@poissonconsulting.ca>
2019-02-13 09:49:24 -0800

    travis

--- R/package.R
+++
@@ -1,9 +0,0 @@
-#' poispkgs
-#'
-#' Attaches CRAN and GitHub R packages used in Poison Consulting scripts in the preferred order.
-#'
-#' @seealso \url{https://github.com/poissonconsulting/poispkgs}
-#' @name poispkgs
-#' @aliases package-poispkgs poispkgs-package
-#' @docType package
-NULL
