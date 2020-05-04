201814c59c4d299ef05735c978598536382f3313
Joe Thorley <joe@poissonconsulting.ca>
2020-05-04 11:25:51 -0700

    wrap text

--- R/pkg-list.R
+++ R/pkg-list.R
@@ -9,13 +9,28 @@ pkg_list <- list(
   bc = c("bcdata", "rems", "wqbc", "wqindex", "fishbc", "fwabc"),
   canada = c("tidyhydat", "canwqdata", "ssdtools"),
   parallel = c("doParallel", "foreach"),
-  misc = c("data.table", "daff", "naniar", "snakecase", "sessioninfo", "units", "yesno"),
+  misc = c(
+    "data.table", "daff", "naniar", "snakecase", "sessioninfo", "units",
+    "yesno"
+  ),
   spatial = c("rgdal", "mapview", "sf"),
-  tidyverse_extras = c("blob", "cli", "crayon", "hms", "glue", "lubridate", "magrittr", "pillar", "readxl", "rlang", "dm"),
-  tidyverse_core = c("ggplot2", "tibble", "tidyr", "readr", "purrr", "dplyr", "stringr", "forcats"),
-  plot = c("grid", "ggplot2", "ggthemes", "ggmap", "ggrepel", "ggspatial", "scales", "viridis", "tinter"),
+  tidyverse_extras = c(
+    "blob", "cli", "crayon", "hms", "glue", "lubridate",
+    "magrittr", "pillar", "readxl", "rlang", "dm"
+  ),
+  tidyverse_core = c(
+    "ggplot2", "tibble", "tidyr", "readr", "purrr", "dplyr",
+    "stringr", "forcats"
+  ),
+  plot = c(
+    "grid", "ggplot2", "ggthemes", "ggmap", "ggrepel", "ggspatial",
+    "scales", "viridis", "tinter"
+  ),
   utils = c("chk", "dttr2", "hmstimer"),
   databasing = c("flobr", "dbflobr", "RSQLite", "readwritesqlite"),
-  analysis = c("universals", "extras", "term", "nlist", "newdata", "rescale", "mcmcr", "mbr", "tmbr", "smbr", "jmbr"),
+  analysis = c(
+    "universals", "extras", "term", "nlist", "newdata", "rescale",
+    "mcmcr", "mbr", "tmbr", "smbr", "jmbr"
+  ),
   reporting = c("subfoldr2", "subreport")
 )
