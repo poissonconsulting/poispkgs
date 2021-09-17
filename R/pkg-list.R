9e728855acb22c6b39dbf3b198eff63840659ccb
Ayla Pearson <ayla@poissonconsulting.ca>
2021-09-17 15:49:20 -0700

    adding readwriteaws to pkg; formating pkg-list a bit

--- R/pkg-list.R
+++ R/pkg-list.R
@@ -1,24 +1,27 @@
 pkg_list <- list(
   rprofile = c(
     "devtools", "fledge", "lintr", "pkgdown", "remotes", "reprex", "rmarkdown",
-    "roxygen2md", "styler", "testthat", "usethis"),
+    "roxygen2md", "styler", "testthat", "usethis"
+  ),
   legacy = c("err", "checkr", "rpdo", "plyr", "reshape2", "poissqlite"),
-  poisutils = c("poisspatial", "poisdata", "poisix", "poisplot", "poisutils", "poispalette"),
+  poisutils = c(
+    "poisspatial", "poisdata", "poisix", "poisplot", "poisutils", "poispalette"
+  ),
   bc = c("bcdata", "rems", "wqbc", "wqindex", "fishbc", "fwapgr", "fwatlasbc"),
   canada = c("canwqdata", "ssdtools", "tidyhydat"),
   parallel = c("doParallel", "foreach"),
   misc = c(
-    "data.table", "daff", "naniar", "snakecase", "sessioninfo", "units",
-    "yesno"
+    "data.table", "daff", "naniar", "readwriteaws", "snakecase", "sessioninfo",
+    "units", "yesno"
   ),
   spatial = c("rgdal", "mapview", "sf"),
   tidyverse_extras = c(
-    "blob", "cli", "crayon", "hms", "glue", "lubridate",
-    "magrittr", "pillar", "readxl", "rlang", "dm"
+    "blob", "cli", "crayon", "hms", "glue", "lubridate", "magrittr", "pillar",
+    "readxl", "rlang", "dm"
   ),
   tidyverse_core = c(
-    "ggplot2", "tibble", "tidyr", "readr", "purrr", "dplyr",
-    "stringr", "forcats"
+    "ggplot2", "tibble", "tidyr", "readr", "purrr", "dplyr", "stringr",
+    "forcats"
   ),
   plot = c(
     "grid", "ggplot2", "ggthemes", "ggmap", "ggrepel", "ggspatial",
